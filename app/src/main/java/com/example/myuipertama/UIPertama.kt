82a1d07354eb07642f2ce53452fdf15f5f40b92c
Viaasyafrina16 <syafrinavia@gmail.com>
2025-10-23 08:56:25 +0700

    Mmebuat column berisi text alamat,copy

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.myuipertama
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
@@ -42,27 +44,44 @@ fun ActivitasPertama(modifier: Modifier){
             .padding(all = 12.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
-            )){
-        Row (){
-        val gambar = painterResource(id = R.drawable.logo_umy)
-            Image(
-                painter = gambar,
-                contentDescription = null,
-                modifier = Modifier.size(size = 100.dp).padding(all = 5.dp)
-            )
-            Spacer(modifier = Modifier.width(width = 30.dp))
-            Column(){
-                Text(
-                    text = stringResource(id = R.string.nama),
-                    fontSize = 30.sp,
-                    fontFamily = FontFamily.Cursive,
-                    color = Color.White,
-                    modifier = Modifier.padding(top = 15.dp)
+            )) {
+            Row() {
+                val gambar = painterResource(id = R.drawable.logo_umy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(size = 100.dp).padding(all = 5.dp)
                 )
-                Text(
-
-                )
+                Spacer(modifier = Modifier.width(width = 30.dp))
+                Column() {
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.alamat),
+                        fontSize = 20.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                }
             }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+
+        ){
+            Text(
+                text = stringResource(id= R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+            )
+        }
 
 
 
